7b0fabf803fff32e1cded03586d53128ec8b51ee
covie <butliveee@yahoo.com>
2022-07-06 02:25:44 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: %i[show create]
+    end
+  end
+end
